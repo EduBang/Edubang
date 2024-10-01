84c481d60c8e00571b9e12a0594636d1494729ef
AS-Pic <sacha@freguin.net>
2024-10-01 09:58:38 +0200

    feat: :sparkles: ajout pas fini de verctoeur avec flèche au dessus

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import math
 
 running = True
 
@@ -17,9 +18,23 @@ class game:
         quit()
         
     def vector(start_pos, end_pos, width = 5):
+        length_vector = math.sqrt(((end_pos[0] - start_pos[0]) ** 2 ) + ((end_pos[1] - start_pos[1]) ** 2))
+        high_triangle = length_vector / 8
+        side_triangle = high_triangle / 2
+        point_1 = (end_pos[0] - high_triangle, end_pos[1] - side_triangle)
+        point_2 = (end_pos[0] + high_triangle, end_pos[1] - side_triangle)
+        
+        pg.draw.line(screen, (0,0,255), end_pos  ,(end_pos[0] - end_pos[0] / 8, end_pos[1] - end_pos[1] / 8),width)
+
+        
         
         pg.draw.line(screen, (0,255,0), start_pos, end_pos, width)
-        pg.draw.polygon(screen, (0,255,0), ((end_pos)) )
+    
+        
+        pg.draw.line(screen, (0,255,0), end_pos, point_1)
+        pg.draw.line(screen, (0,255,0), end_pos, point_2)
+        
+        
         
 
 while running:
@@ -27,7 +42,7 @@ while running:
     for event in pg.event.get():
         
         pg.draw.circle(screen, (255,0,0), (w / 2, h / 2), 30, 0)
-        game.vector((10, 10), (20, 20), 1)
+        game.vector((10, 10), (200, 200), 1)
         
         if event.type == pg.QUIT:
             running = False
