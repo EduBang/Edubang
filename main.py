bfe66409c0cc8fcd463bf53490df1edb449863a1
AS-Pic <sacha@freguin.net>
2024-10-01 08:14:21 +0200

    feat: :rocket: Boucle de jeu

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-test
+import pygame as pg
+
+running = True
+
+resolution = (1000, 1000)
+screen = pg.display.set_mode(resolution)
+black = (0, 0, 0)
+
+class game:
+
+    def draw_screen():
+        screen.fill(black)
+        pg.display.update()
+
+    def quit_algo():
+        pg.quit()
+        quit()
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+
+
+
+    game.draw_screen()
+
+game.quit_algo()
