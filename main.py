8d21da866fe4886aeaf52120423d775c879b4cd9
AS-Pic <sacha@freguin.net>
2024-10-01 17:37:02 +0200

    feat: :zap: création classe intems pour organiser le code

--- main.py
+++ main.py
@@ -14,28 +14,33 @@ pg.display.set_caption('EduBang')
 icon = pg.image.load('icon.png')
 pg.display.set_icon(icon)
 
-class game:
-
-    @staticmethod
-    def draw_screen():
-        pg.display.update()
-
-    @staticmethod
-    def quit_algo():
-        pg.quit()
-        quit()
-
-    @staticmethod
+class items:
+    
     def vector(start_pos, end_pos, width=5):
         # Dessiner le vecteur vert
         pg.draw.line(screen, (0, 255, 0), start_pos, end_pos, width)
 
-        # Calcul de la direction opposée pour le vecteur bleu
-        # La direction opposée est obtenue en inversant les coordonnées
+
         opposite_pos = (end_pos[0] - 0.2 * (end_pos[0] - start_pos[0]), end_pos[1] - 0.2 * (end_pos[1] - start_pos[1]))
 
-        # Dessiner le vecteur bleu dans la direction opposée
+
         pg.draw.line(screen, (0, 0, 255), end_pos, opposite_pos, width)
+    
+
+
+
+
+class game:
+
+
+    def draw_screen():
+        pg.display.update()
+
+    def quit_algo():
+        pg.quit()
+        quit()
+
+    
 
 
 while running:
@@ -44,17 +49,15 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    # Remplir l'écran en noir
     screen.fill(black)
 
-    # Dessiner un cercle rouge au centre de l'écran
     pg.draw.circle(screen, (255, 0, 0), (w / 2, h / 2), 30, 0)
 
 
-    game.vector((200, 200), (400, 200), 5)
+    items.vector((200, 200), (400, 200), 5)
 
 
     game.draw_screen()
 
-# Quitter proprement
+
 game.quit_algo()
