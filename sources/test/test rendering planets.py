236226989571308c9bdfb858ba447e573a2be0fe
AS-Pic <sacha@freguin.net>
2025-01-06 14:53:52 +0100

    feat: :sparkles: ajout perlin sphère

--- sources/test/test rendering planets.py
+++ sources/test/test rendering planets.py
@@ -1,42 +1,38 @@
 import pygame as pg
 from math import *
 from nsi25perlin import PerlinNoise as perlin 
+import opensimplex
 
-Perlin_surface = perlin(200)
+                
+planet_pos = (500, 500) # Position du centre de la planète
+radius = 100 # Rayon de la planète
+Perlin_surface = perlin(radius * 2)
 liste = []
 
 def generer_perlin():
     for x in range(Perlin_surface.size):
         liste.append([])
         for y in range(Perlin_surface.size):
-            value = Perlin_surface.noise(x / 2000, y / 2000)
+            value = Perlin_surface.noise(x / 100, y / 100)
             liste[x].append(value * 255)
     return liste
 
-
-
-
-def draw_perlin(liste, screen, pos_x = 640, pos_y = 300, radius = 100):
+def draw_perlin(liste, screen, planet_pos, radius):
+    start_pos = (planet_pos[0] - radius, planet_pos[1] - radius)
 
     for x, element in enumerate(liste):
         for y, value in enumerate(element):
+            pixel_pos = (x + start_pos[0], y + start_pos[1])
 
-            if sqrt((x + radius / 2) ** 2 + (y + radius / 2) ** 2) <= radius:
-                screen.set_at((int(x + pos_x + radius / 2), int(y + pos_y + radius / 2)),(abs(value), abs(value), abs(value)))
-                
-
-
-
-
-
-
+            if sqrt((planet_pos[0] - pixel_pos[0]) ** 2 + (planet_pos[1] - pixel_pos[1]) ** 2) <= radius:
+                screen.set_at((int(x + start_pos[0]), int(y + start_pos[1])),(abs(value), abs(value), abs(value)))
 
 
 
 
 def main():
    
-    screen = pg.display.set_mode((1280, 720))  # Initialiser la fenêtre d'affichage
+    screen = pg.display.set_mode((1000, 1000))  # Initialiser la fenêtre d'affichage
     pg.display.set_caption("Test Rendering Planets")
     clock = pg.time.Clock()
     running = True
@@ -49,8 +45,7 @@ def main():
         screen.fill((0, 0, 0))
 
         generer_perlin()
-        draw_perlin(liste, screen, (630, 300))
-
+        draw_perlin(liste, screen, planet_pos, radius)
         
         pg.display.flip()
         clock.tick(60)  
